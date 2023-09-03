0bab9251d84a77cd75c2ac6d1f2abd8c9c6edbc3
J-Dubois <134377045+J-Dubois@users.noreply.github.com>
2023-09-03 20:27:55 +0200

    Update QR_CODE_Generator.py

--- QR_CODE_Generator.py
+++ QR_CODE_Generator.py
@@ -1,14 +1,14 @@
-import qrcode.constants #fait le lien avec la librairie nécessaire
+import qrcode.constants                                          #make link with necessary library
 
-qr = qrcode.QRCode(  #créer la variable qr qui utilise le module importé -> qrcode et est caractérisé par la fonction QRCODE
-    version=3,       #de 1 à 10, barème de complexité des carrés noir du qr code sur l'image
+qr = qrcode.QRCode(                                              #create the variable (qr) which use the imported module (qrcode) and caracterise by the QRCODE function
+    version=3,                                                   #from 1 to 10, its a complexity rate which impact the qr code black squares on the final picture
     box_size=5,      
     border=5,
 )
 
-qr.add_data('put data')   #on insère dans notre variable nos données (url/images)
-qr.make(fit=True)         #on précise que la variable est forcément vraie
+qr.add_data('put data')                                          #we add our data (url/picture) inside the variable
+qr.make(fit=True)                                                #we precise that the variable is always true
 
-img = qr.make_image(fill_color="black", back_color="white")      #permet de gérer les couleurs du qr code
+img = qr.make_image(fill_color="black", back_color="white")      #this allow to change the color of the qr code and the background
 
 img.save('QR_name.png')   #génère le fichier png
